deb8fc43478a4e52635f098989fdbe479d8e6aab
Jonhnatha Trigueiro <joepreludian@gmail.com>
2014-07-15 13:03:54 -0300

    small adjusts in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import find_packages
 
 setup(
     name='deployer',
-    version='0.0.3',
-    packages=['deployer'],
-    package_dir={'deployer': 'deployer'},
-    package_data={'mypkg': ['deployer/template/*.conf']},
+    version='0.0.4',
+    packages=find_packages(),
+    package_data={'deployer': ['deployer/template/*.conf']},
     url='http://joey.universo42.com.br',
     license='MIT',
     install_requires=['colorama', 'argparse', 'virtualenv'],
