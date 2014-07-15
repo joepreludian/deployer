cb88d389bc8483bb729246d885cb18d7289cb944
Jonhnatha Trigueiro <joepreludian@gmail.com>
2014-07-15 09:53:10 -0300

    Added package info in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import find_packages
 
 setup(
     name='deployer',
-    version='0.0.2',
-    packages=find_packages(),
+    version='0.0.3',
+    packages=['deployer'],
+    package_dir={'deployer': 'deployer'},
+    package_data={'mypkg': ['deployer/template/*.conf']},
     url='http://joey.universo42.com.br',
     license='MIT',
     install_requires=['colorama', 'argparse', 'virtualenv'],
     author='Jonhnatha Trigueiro',
     author_email='joepreludian@gmail.com',
-    description='A handful tool to manage multiple django apps',
+    description='A handful tool to install and manage multiple django apps',
     entry_points={
         'console_scripts': [
             'deployer = deployer.cli:main',
